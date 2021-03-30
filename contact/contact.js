9747546e425c338cafd1a11000277c3b9adadb3c
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-03-30 15:11:56 +0200

    portfolio

--- contact/contact.js
+++ contact/contact.js
@@ -1,41 +1,6 @@
-let monBouton=document.querySelector(".bouton2")
-let phone=document.getElementById("phone")
-let mail=document.getElementById("mail")
-let div1=document.querySelector(".div1")
-let div2=document.querySelector(".div2")
 
-window.addEventListener('load',()=>{
-    div2.innerHTML="contactez moi par mail à l'adresse tristan3755@gmail.com ou en cliquant sur l'icone"
-})
 
-monBouton.addEventListener('click',()=>{
-    if(monBouton.style.left<="0%"){
-    monBouton.style.left="30%"
-    mail.style.color="white"
-    phone.style.color="#f8d50c"
-    div1.style.backgroundImage="url(none)"
-    div1.innerHTML="contactez moi par téléphone au 0771822320 ou en cliquant sur l'icone"
-    div2.innerHTML=""
-    div2.style.backgroundImage="url(./images/contact.jpg)"
-    }else if(monBouton.style.left>="30%"){
-        monBouton.style.left="0%"
-        mail.style.color="#f8d50c"
-        phone.style.color="white"
-        div1.style.backgroundImage="url(./images/mail.jpg)"
-        div1.innerHTML=""
-        div2.style.backgroundImage="url(none)"
-        div2.innerHTML="contactez moi par mail à l'adresse tristan3755@gmail.com ou en cliquant sur l'icone"
-    }
-})
 
-let monTrou=document.querySelector(".trou")
-monBouton.addEventListener('click',()=>{
-    if(monBouton.style.left<="0%"){
-        monTrou.style.left="60%"
-        }else if(monBouton.style.left>="30%"){
-            monTrou.style.left="40%"
-        }
-})
 
 /******************************menu ******************/
 let maCroix=document.getElementById('croix')
