5e2e9dc26d023375a3a5fd5c8dd0c1b28f156f13
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-04-05 12:12:39 +0200

    portfolio

--- qui/javascript.js
+++ qui/javascript.js
@@ -199,3 +199,27 @@ contact.addEventListener('mouseover',()=>{
 contact.addEventListener('mouseout',()=>{
   legendeContact.style.display='none'
 })
+/*********************formContact *******************************/
+
+let maCroixContact=document.getElementById('croixContact')
+let contactForm=document.getElementById('contactForm')
+let articles=document.querySelector('.contactArticle')
+let articles1=document.querySelector('.contactArticle1')
+
+maCroixContact.addEventListener('click',()=>{
+contactForm.style.opacity="0"
+contactForm.style.zIndex="0"
+contactForm.style.width="0%"
+articles.style.opacity='0'
+articles1.style.opacity='0'
+})
+
+  let boutonContactMenu=document.getElementById('contact')
+  boutonContactMenu.addEventListener('click',()=>{
+  contactForm.style.opacity="1"
+  contactForm.style.zIndex="50"
+  contactForm.style.width="100%"
+  articles.style.opacity='1'
+  articles1.style.opacity='1'
+  
+  })
