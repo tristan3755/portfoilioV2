58433b465da4752d0f4d94e78316b3c69f1b5d66
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-04-05 12:19:01 +0200

    portfolio

--- portfolio/javascript.js
+++ portfolio/javascript.js
@@ -270,3 +270,27 @@ let path=document.querySelectorAll('svg path')
     console.log(`lettre ${i} is ${path[i].getTotalLength()}`)
   }
 
+/*********************formContact *******************************/
+
+let maCroixContact=document.getElementById('croixContact')
+let contactForm=document.getElementById('contactForm')
+let articles=document.querySelector('.contactArticle')
+let articles1=document.querySelector('.contactArticle1')
+
+maCroixContact.addEventListener('click',()=>{
+contactForm.style.opacity="0"
+contactForm.style.zIndex="0"
+contactForm.style.width="0%"
+articles.style.opacity='0'
+articles1.style.opacity='0'
+})
+
+  let boutonContactMenu=document.getElementById('contact')
+  boutonContactMenu.addEventListener('click',()=>{
+  contactForm.style.opacity="1"
+  contactForm.style.zIndex="50"
+  contactForm.style.width="100%"
+  articles.style.opacity='1'
+  articles1.style.opacity='1'
+  
+  })
