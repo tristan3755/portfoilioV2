157f27d59b49ee1dcf782230d20c7b174c1b15b9
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-04-06 15:30:50 +0200

    portfolio

--- javascript.js
+++ javascript.js
@@ -14,14 +14,14 @@ let contactForm=document.getElementById('contactForm')
 let articles=document.querySelector('.contactArticle')
 let articles1=document.querySelector('.contactArticle1')
 
+
 maCroixContact.addEventListener('click',()=>{
 contactForm.style.opacity="0"
 contactForm.style.zIndex="0"
 contactForm.style.width="0%"
 articles.style.opacity='0'
 articles1.style.opacity='0'
-articles.style.width='0%'
-articles1.style.width='0%'
+
 })
 let boutonContact=document.querySelector('.contactMenu')
 boutonContact.addEventListener('click',()=>{
@@ -30,8 +30,8 @@ boutonContact.addEventListener('click',()=>{
   contactForm.style.width="100%"
   articles.style.opacity='1'
   articles1.style.opacity='1'
-  articles.style.width='40%'
-articles1.style.width='40%'
+
+
 
   })
 
